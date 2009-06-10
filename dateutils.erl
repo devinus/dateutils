646a45bbc0ed385c897dff0dbfec2277016920bc
Jonas Enlund <jonas.enlund@gmail.com>
2009-06-10 22:13:29 +0300

    Added a few tests. Some more work on the parser

--- dateutils.erl
+++ dateutils.erl
@@ -32,17 +32,24 @@
 	 today/0, tomorrow/0, yesterday/0
 	]).
 
--define(FULL_YEAR, "Y").
--define(DAY_OF_MONTH_WITH_LEADING_ZEROES, "d").
--define(DAY_OF_MONTH_WITHOUT_LEADING_ZEROES, "D").
--define(MONTH_WITH_LEADING_ZEROES, "m").
--define(MONTH_WITHOUT_LEADING_ZEROES, "M").
--define(HOUR_WITH_LEADING_ZEROES, "h").
--define(HOUR_WITHOUT_LEADING_ZEROES, "H").
--define(MINUTE_WITH_LEADING_ZEROES, "i").
--define(MINUTE_WITHOUT_LEADING_ZEROES, "I").
--define(SECOND_WITH_LEADING_ZEROES, "s").
--define(SECOND_WITHOUT_LEADING_ZEROES, "S").
+-include_lib("eunit/include/eunit.hrl").
+
+-define(FULL_YEAR,                           "<Y>").
+-define(MONTH_WITH_LEADING_ZEROES,           "<m>").
+-define(MONTH_WITHOUT_LEADING_ZEROES,        "<M>").
+-define(WEEK_OF_YEAR_WITH_LEADING_ZEROES,    "<wy>").
+-define(WEEK_OF_YEAR_WITHOUT_LEADING_ZEROES, "<WY>").
+-define(DAY_OF_YEAR_WITH_LEADING_ZEROES,     "<dy>").
+-define(DAY_OF_YEAR_WITHOUT_LEADING_ZEROES,  "<DY>").
+-define(DAY_OF_MONTH_WITH_LEADING_ZEROES,    "<d>").
+-define(DAY_OF_MONTH_WITHOUT_LEADING_ZEROES, "<D>").
+-define(DAY_OF_WEEK,                         "<WD>").
+-define(HOUR_WITH_LEADING_ZEROES,            "<h>").
+-define(HOUR_WITHOUT_LEADING_ZEROES,         "<H>").
+-define(MINUTE_WITH_LEADING_ZEROES,          "<mi>").
+-define(MINUTE_WITHOUT_LEADING_ZEROES,       "<MI>").
+-define(SECOND_WITH_LEADING_ZEROES,          "<s>").
+-define(SECOND_WITHOUT_LEADING_ZEROES,       "<S>").
 
 
 
@@ -66,13 +73,13 @@ parse_date(?FULL_YEAR ++ XS, YS, Dict) ->
     parse_date(XS, Rest, dict:append(year, YYYY, Dict));
 
 
-% Month, with leading zeros
+% Month, with leading zeroes
 parse_date(?MONTH_WITH_LEADING_ZEROES ++ XS, YS, Dict) ->
     {ok, MM, Rest} = parse(2, digits, YS),
     parse_date(XS, Rest, dict:append(month, MM, Dict));
 
 
-% Month, without leading zeros
+% Month, without leading zeroes
 parse_date(?MONTH_WITHOUT_LEADING_ZEROES ++ XS, YS, Dict) ->
     case parse(2, digits, YS) of
 	{ok, MM, Rest} -> parse_date(XS, Rest, dict:append(month, MM, Dict));
@@ -80,8 +87,36 @@ parse_date(?MONTH_WITHOUT_LEADING_ZEROES ++ XS, YS, Dict) ->
 			  parse_date(XS, Rest, dict:append(month, MM, Dict))
     end;
 
+% Week of year, with leading zeroes
+parse_date(?WEEK_OF_YEAR_WITH_LEADING_ZEROES ++ XS, YS, Dict) ->
+    {ok, WW, Rest} = parse(2, digits, YS),
+    parse_date(XS, Rest, dict:append(week_of_year, WW, Dict));
 
-% Day of the month, with leading zeros
+% Week of year, without leading zeroes
+parse_date(?WEEK_OF_YEAR_WITHOUT_LEADING_ZEROES ++ XS, YS, Dict) ->
+    case parse(2, digits, YS) of
+	{ok, WW, Rest} -> parse_date(XS, Rest, dict:append(week_of_year, WW, Dict));
+	{error, _}     -> {ok, WW, Rest} = parse(1, digits, YS),
+			  parse_date(XS, Rest, dict:append(week_of_year, WW, Dict))
+    end;
+
+% Day of year, with leading zeroes
+parse_date(?DAY_OF_YEAR_WITH_LEADING_ZEROES ++ XS, YS, Dict) ->
+    {ok, DOY, Rest} = parse(3, digits, YS),
+    parse_date(XS, Rest, dict:append(day_of_year, DOY, Dict));
+
+% Day of year, without leading zeroes
+parse_date(?DAY_OF_YEAR_WITHOUT_LEADING_ZEROES ++ XS, YS, Dict) ->
+    case parse(3, digits, YS) of
+	{ok, DOY, Rest} -> parse_date(XS, Rest, dict:append(day_of_year, DOY, Dict));
+	{error, _} -> case parse(2, digits, YS) of
+			  {ok, DOY, Rest} -> parse_date(XS, Rest, dict:append(day_of_year, DOY, Dict));
+			  {error, _} -> {ok, DOY, Rest} = parse(1, digits, YS),
+					parse_date(XS, Rest, dict:append(day_of_year, DOY, Dict))
+		      end
+    end;
+
+% Day of the month, with leading zeroes
 parse_date(?DAY_OF_MONTH_WITH_LEADING_ZEROES ++ XS, YS, Dict) ->
     {ok, DD, Rest} = parse(2, digits, YS),
     parse_date(XS, Rest, dict:append(day_of_month, DD, Dict));
@@ -95,6 +130,11 @@ parse_date(?DAY_OF_MONTH_WITHOUT_LEADING_ZEROES ++ XS, YS, Dict) ->
 			  parse_date(XS, Rest, dict:append(day_of_month, DD, Dict))
     end;
 
+% Day of week, 1 digit
+parse_date(?DAY_OF_WEEK ++ XS, YS, Dict) ->
+    {ok, DW, Rest} = parse(1, digits, YS),
+    parse_date(XS, Rest, dict:append(day_of_week, DW, Dict));
+
 % Literal characters must be identical. 
 parse_date([X|XS], [X|YS], Dict) ->
     parse_date(XS, YS, Dict);
@@ -104,8 +144,42 @@ parse_date([], [], Dict) ->
     Dict.
 
 
-build_date(DateDict, Defaults)->
-    DateDict.
+build_date(DateDict, Defaults) ->
+    
+    % The year is built first. there can be
+    % no conflict here.
+    Year = case dict:find(year, DateDict) of
+	       {ok, [Y|_]} -> Y;
+	       error -> apply(dict:fetch(year, Defaults), [])
+	   end,
+
+    % If the day of year is in the DateDict, we use it and we are done.
+    case dict:find(day_of_year, DateDict) of
+	{ok, [DOY|_]} -> {Date, _} = add({{Year,1,1},{12,0,0}}, DOY-1),
+			 Date;
+	error ->
+	    Month = case dict:find(month, DateDict) of
+			{ok, [M|_]} -> M;
+			error -> apply(dict:fetch(month, Defaults), [])
+		    end,
+    
+	    Day = case dict:find(day_of_month, DateDict) of
+		      {ok, [D|_]} -> D;
+		      error -> apply(dict:fetch(day_of_month, Defaults), [])
+		  end,
+	    {Year, Month, Day}
+    end.
+
+build_time(DateDict, Defaults) ->
+    Hour = apply(dict:fetch(hour, Defaults), []),	    
+    Minute = apply(dict:fetch(minute, Defaults), []),
+    Second = apply(dict:fetch(second, Defaults), []),
+    {Hour,Minute,Second}.
+
+build_datetime(DateDict, Defaults) ->
+    Date = build_date(DateDict, Defaults),
+    Time = build_time(DateDict, Defaults),
+    {Date, Time}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% make_parser/1 returns a parser that parses dates
@@ -120,22 +194,23 @@ build_date(DateDict, Defaults)->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 make_parser(Format, Defaults) ->
     fun(DateString) ->
-	    DateDict0 = dict:new(),
-	    DateDict = parse_date(Format, DateString, DateDict0),
-	    build_date(DateDict, Defaults)
+	    DateDict = parse_date(Format, DateString, dict:new()),
+	    build_datetime(DateDict, Defaults)
     end.
 
 make_parser(Format) ->
     Defaults = dict:from_list(
-		 [{year,  fun() -> year (today()) end},
+		 [{year, fun() -> year(today()) end},
+		  {week_of_year, fun() -> 1 end},
 		  {month, fun() -> month(today()) end},
-		  {day,   fun() -> day  (today()) end}
+		  {day_of_year, fun() -> 1 end},
+		  {day_of_month, fun() -> day(today()) end}, % How to fix this?
+		  {day_of_week, fun() -> 1 end},
+		  {hour, fun() -> 12 end},
+		  {minute, fun() -> 0 end},
+		  {second, fun() -> 0 end}
 		 ]),
     make_parser(Format, Defaults).
-
-year ({{YYYY, _, _},_}) -> YYYY.
-month({{   _,MM, _},_}) -> MM.
-day  ({{   _, _,DD},_}) -> DD.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% make_writer/1 creates a printer which prints dates
@@ -239,6 +314,9 @@ iso8601Week(DateTime) ->
     Days = calendar:date_to_gregorian_days(Thursday) - calendar:date_to_gregorian_days({YYYY, 1, 1}),
     Days div 7 + 1.
 
+year ({{YYYY, _, _},_}) -> YYYY.
+month({{   _,MM, _},_}) -> MM.
+day  ({{   _, _,DD},_}) -> DD.
 
     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
---
+++ dateutils_tests.erl
@@ -0,0 +1,28 @@
+-module(dateutils_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+% First I try to parse the different dateformats from 
+% http://en.wikipedia.org/wiki/ISO_8601
+
+% 2009-06-10, represented as <Y>-<m>-<d>
+simple_date_test() ->
+    Parse = dateutils:make_parser("<Y>-<m>-<d>"),
+    {{2009,6,10},_} = Parse("2009-06-10").
+
+% Same as above, but without leading zeroes
+simple_date2_test() ->
+    Parse = dateutils:make_parser("<Y>-<M>-<D>"),
+    {{2009,6,10}, _} = Parse("2009-6-10").
+
+
+% 2009-161, represented as <Y>-<dy>
+ordinal_date_test() ->
+    Parse = dateutils:make_parser("<Y>-<dy>"),
+    io:format("<Y>-<dy>: ~p~n", [Parse("2009-161")]).
+
+% Same as above, without leading zeroes
+ordinal_date2_test() ->
+    Parse = dateutils:make_parser("<Y>-<DY>"),
+    io:format("<Y>-<DY>: ~p~n", [Parse("2008-64")]).
+
